2897422c4b9d71f041e9406986d91014409b4957
Pedro Paulo Carvalho Vieira <ppaulo030601@gmail.com>
2022-11-28 21:28:03 -0300

    Minor bugs fixed

--- get_dir.py
+++ get_dir.py
@@ -2,4 +2,4 @@ import os
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 def get_dir(path):
-    return os.path.join(THIS_DIR, path)
+    return os.path.join(THIS_DIR, path).replace("\\", "/")
